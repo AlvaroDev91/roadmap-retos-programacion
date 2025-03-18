ff6f9668d1aefcae12002d0f86ea1e25c0d9c820
AlvaroDev <alvaro.cg91@gmail.com>
2025-03-18 17:34:02 +0100

    ejercicio 2 AlvaroDev91

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/alvaroDev91.py
@@ -0,0 +1,69 @@
+'''
+/*
+ * EJERCICIO:
+ * - Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+ *   Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
+ *   (Ten en cuenta que cada lenguaje puede poseer unos diferentes)
+ * - Utilizando las operaciones con operadores que tú quieras, crea ejemplos
+ *   que representen todos los tipos de estructuras de control que existan
+ *   en tu lenguaje:
+ *   Condicionales, iterativas, excepciones...
+ * - Debes hacer print por consola del resultado de todos los ejemplos.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea un programa que imprima por consola todos los números comprendidos
+ * entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+ *
+ * Seguro que al revisar detenidamente las posibilidades has descubierto algo nuevo.
+ */
+'''
+
+variable1 = 5
+variable2 = 8
+
+#1:
+print(variable1 + variable2)
+print(variable1 - variable2)
+print(variable1 // variable2)
+print(variable1 / variable2)
+print(variable1 * variable2)
+print(variable1 ** variable2)
+print(variable1 % variable2)
+
+print(variable1 == variable2)
+print(variable1 != variable2)
+print(variable1 > variable2)
+print(variable1 >= variable2)
+print(variable1 <= variable2)
+print(variable1 < variable2)
+
+print(variable1 and variable2 == 12)
+print(variable1 + 5 == 10 or variable2 + 2 == 10)
+print(not variable1 + variable2 + 2 == 10)
+
+variable1 = 5
+variable2 = 8
+print(variable1)
+
+variable1 += 1
+print(variable1)
+
+variable1 -= 1
+print(variable1)
+
+variable1 += 2
+print(variable1)
+
+variable1 /= 2
+print(variable1)
+
+
+variable1 %= 2
+print(variable1)
+
+variable1 **= 2
+print(variable1)
+
+variable1 //= 2
+print(variable1)
+
